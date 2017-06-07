bf4d880d470a63c8ccb0f8a7b0a92a38546e4494
Woodson Delhia <woodsondelhia88@mail.com>
2017-06-06 20:48:36 -0400

    added testing and small change to mine function

--- lib/basic_blockchain.ex
+++ lib/basic_blockchain.ex
@@ -1,14 +1,18 @@
 defmodule BasicBlockchain do
-   defstruct index: 0, previousHash: "0", timestamp: :os.system_time(:millisecond), data: :none, hash: "0", nonce: 0
-   @nonce_limit 500000
+   defstruct index: 0, previousHash: "0", timestamp: :os.system_time(:millisecond), data: [], hash: "0", nonce: 0
+   #@nonce_limit 10000000
 
    def create_blockchain do
      Agent.start_link(fn -> [generate_genesis_block()] end)
    end
 
    def generate_genesis_block do
-      struct(%BasicBlockchain{}, %{data: "Genesis Block"})
+      struct(%BasicBlockchain{}, %{data: "Genesis Block", nonce: generate_nonce()})
       |> generate_block_hash([])
+   end
+
+   defp generate_nonce do
+     :rand.uniform(:os.system_time(:millisecond) |> Integer.digits |> Enum.take(-4) |> Integer.undigits)
    end
 
    def get_blockchain(blockchain) do
@@ -40,7 +44,7 @@ defmodule BasicBlockchain do
         next_timestamp = :os.system_time(:millisecond)
         next_index =  previous_block.index + 1
 
-        struct(%BasicBlockchain{}, index: next_index, timestamp: next_timestamp, previousHash: previous_block.hash, data: blockdata )
+        struct(%BasicBlockchain{}, index: next_index, timestamp: next_timestamp, previousHash: previous_block.hash, data: blockdata, nonce:  generate_nonce())
         |> generate_block_hash(blockchain)
    end
 
@@ -50,7 +54,7 @@ defmodule BasicBlockchain do
 
    def calculate_hash(block, blckchn) when is_map(block) do
      blockchain = if blckchn == [], do: blckchn, else: get_blockchain(blckchn)
-     value = (blockchain |> :erlang.term_to_binary) <> (block.index |> Integer.to_string) <> block.previousHash <> (block.nonce |> Integer.to_string) <> (block.timestamp |> Integer.to_string) <> (block.data |> :erlang.term_to_binary)
+     value = (blockchain |> :erlang.term_to_binary) <> (block.index |> to_string) <> block.previousHash <> (block.nonce |> to_string) <> (block.timestamp |> to_string) <> (block.data |> :erlang.term_to_binary)
 
      :crypto.hash_init(:sha256)
      |> :crypto.hash_update(value)
@@ -72,33 +76,29 @@ defmodule BasicBlockchain do
    end
 
    def mine(blockchain, block) do
-      cond do
-        block.nonce == @nonce_limit ->
-          {:error, "could not mine block."}
-        {:success, mined_block, _} =  mining(blockchain, block) ->
-          :ok = update_block_from_chain(blockchain, mined_block)
-      end
+     {:success, mined_block, _} =  mining(blockchain, block)
    end
 
    def mining(blockchain, block) do
-     inc_nonce = nonce + 1
-     mine_attempt = calculate_hash(block, blockchain)
-     if mine_attempt |> String.starts_with?("0000"), do: {:success, %{block | hash: mine_attempt, nonce: inc_nonce}, "Mine completed!"}, else: mine(blockchain, %{block | nonce: inc_nonce})
+       block = %{block | nonce:  block.nonce + 1}
+       mine_attempt = calculate_hash(block, blockchain)
+       if mine_attempt |> String.starts_with?("0000"), do: {:success, %{block | hash: mine_attempt}, "Mine completed!"}, else: mine(blockchain, block)
    end
-
 end
 
 # {:ok, blockchain} = BasicBlockchain.create_blockchain
 # genesis_block = BasicBlockchain.get_blockchain(blockchain)
-# transaction_data = %{sender: "Ikumi", recepient: "Woodson", amount: 35, payment: "Debit" }
-# ts_bl1 = BasicBlockchain.generate_next_block(transaction_data, blockchain)
+  # transaction_data = %{sender: "Ikumi", recepient: "Woodson", amount: 35, payment: "Debit" }
+  # ts_bl1 = BasicBlockchain.generate_next_block(transaction_data, blockchain)
 #  BasicBlockchain.add_block_to_chain(blockchain, ts_bl1)
 # transaction_data2 = %{sender: "Woodson", recepient: "Jawaad", amount: 10, payment: "Debit" }
-# ts_bl1 = BasicBlockchain.generate_next_block(transaction_data2, blockchain)
+# ts_bl2 = BasicBlockchain.generate_next_block(transaction_data2, blockchain)
 #  BasicBlockchain.add_block_to_chain(blockchain, ts_bl2)
 #  blocks = BasicBlockchain.get_blockchain(blockchain)
+# [update_block | _] = blocks
+# updated_block = Map.from_struct(update_block) |> Kernel.put_in([:data,:amount], 15)
 # %{index: 0, previousHash: "0", timestamp: :os.system_time(:millisecond), data: "my genesis!", hash: "U1iquFMbMopg1jG+Vqtn/h4jPQEE6hGnWgNoj0nRr5Ez1PlQ8XSpFDDeWmXChPN5"}
 # h = BasicBlockchain.calculate_hash(b)
-# block = BasicBlockchain.create(%{b | hash: h})
+# block = BasicBlockchain.create(%{b | hash: hblocks = BasicBlockchain.get_blockchain(blockchain)})
 # transaction_data = %{sender: "Ikumi", recepient: "Woodson", amount: 35, payment: "Debit" }
 # BasicBlockchain.generate_next_block(transaction_data, blockchain)
--- test/basic_blockchain_test.exs
+++ test/basic_blockchain_test.exs
@@ -2,7 +2,43 @@ defmodule BasicBlockchainTest do
   use ExUnit.Case
   doctest BasicBlockchain
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    {:ok, blockchain} = BasicBlockchain.create_blockchain
+    {:ok, [blockchain: blockchain]}
+  end
+
+  test "Genenis block create", context do
+    chain = BasicBlockchain.get_blockchain(context[:blockchain])
+
+    assert length(chain) == 1
+    assert List.first(chain) |> Map.get(:data) |> Kernel.==("Genesis Block")
+  end
+
+  test "Create a block", context do
+    ts_bl1 = %{sender: "Ikumi", recepient: "Woodson", amount: 35, payment: "Debit" }
+             |> BasicBlockchain.generate_next_block(context[:blockchain])
+    assert :ok = BasicBlockchain.add_block_to_chain(context[:blockchain], ts_bl1)
+  end
+
+  test "Nonce generated always unique on blocks", context do
+    nonce_list = Enum.map(1..10, fn x ->  generate_block(x, context) |> Map.get(:nonce) end)
+    uniq_nonce_list = Enum.uniq(nonce_list)
+    assert nonce_list == uniq_nonce_list
+  end
+
+  test "Mining function return block with a new nonce and ok status", context do
+     Enum.map(1..3, fn x ->  block = generate_block(x, context)
+        assert :ok = BasicBlockchain.add_block_to_chain(context[:blockchain], block)
+     end)
+     [update_block | _] = BasicBlockchain.get_blockchain(context[:blockchain])
+     updated_block = Map.from_struct(update_block) |>  Kernel.put_in([:data,:amount], 100)
+     {:success, mined_block, _} = BasicBlockchain.mine(context[:blockchain], updated_block)
+     assert :ok = BasicBlockchain.update_block_from_chain(context[:blockchain], mined_block)
+  end
+
+  def generate_block(n, context) do
+    n_to_s = to_string(n)
+    %{sender: "Ikumi"<> n_to_s, recepient: "Woodson" <> n_to_s, amount: 35 + n, payment: "Debit" }
+    |> BasicBlockchain.generate_next_block(context[:blockchain])
   end
 end
